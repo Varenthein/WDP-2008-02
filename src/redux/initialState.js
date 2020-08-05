33bf309f60397ea29c2f19b3f9eaed2968f62bca
Tomasz Rosik <Bosik27@gmail.com>
2020-08-05 14:59:08 +0200

    Add props image to ProductBox updated initailState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -10,6 +10,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-1',
       name: 'Aenean Ru Bristique 1',
+      image:
+        'https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -19,6 +21,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-2',
       name: 'Aenean Ru Bristique 2',
+      image:
+        'https://images.pexels.com/photos/279618/pexels-photo-279618.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -28,6 +32,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-3',
       name: 'Aenean Ru Bristique 3',
+      image:
+        'https://images.pexels.com/photos/775219/pexels-photo-775219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -37,6 +43,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-4',
       name: 'Aenean Ru Bristique 4',
+      image:
+        'https://images.pexels.com/photos/1148955/pexels-photo-1148955.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -46,6 +54,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-5',
       name: 'Aenean Ru Bristique 5',
+      image:
+        'https://images.pexels.com/photos/220749/pexels-photo-220749.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -55,6 +65,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-6',
       name: 'Aenean Ru Bristique 6',
+      image:
+        'https://images.pexels.com/photos/827518/pexels-photo-827518.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -64,6 +76,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-7',
       name: 'Aenean Ru Bristique 7',
+      image:
+        'https://images.pexels.com/photos/1321290/pexels-photo-1321290.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -73,6 +87,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-8',
       name: 'Aenean Ru Bristique 8',
+      image:
+        'https://images.pexels.com/photos/945688/pexels-photo-945688.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -82,6 +98,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-9',
       name: 'Aenean Ru Bristique 9',
+      image:
+        'https://images.pexels.com/photos/462235/pexels-photo-462235.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -91,6 +109,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-10',
       name: 'Aenean Ru Bristique 10',
+      image:
+        'https://images.pexels.com/photos/3554424/pexels-photo-3554424.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -100,6 +120,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-11',
       name: 'Aenean Ru Bristique 11',
+      image:
+        'https://images.pexels.com/photos/271743/pexels-photo-271743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -109,6 +131,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-12',
       name: 'Aenean Ru Bristique 12',
+      image:
+        'https://images.pexels.com/photos/262048/pexels-photo-262048.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -118,6 +142,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-13',
       name: 'Aenean Ru Bristique 13',
+      image:
+        'https://images.pexels.com/photos/276528/pexels-photo-276528.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -127,6 +153,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-14',
       name: 'Aenean Ru Bristique 14',
+      image:
+        'https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -136,6 +164,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-15',
       name: 'Aenean Ru Bristique 15',
+      image:
+        'https://images.pexels.com/photos/279618/pexels-photo-279618.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -145,6 +175,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-16',
       name: 'Aenean Ru Bristique 16',
+      image:
+        'https://images.pexels.com/photos/775219/pexels-photo-775219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -154,6 +186,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-17',
       name: 'Aenean Ru Bristique 17',
+      image:
+        'https://images.pexels.com/photos/1148955/pexels-photo-1148955.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -163,6 +197,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-18',
       name: 'Aenean Ru Bristique 18',
+      image:
+        'https://images.pexels.com/photos/220749/pexels-photo-220749.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -172,6 +208,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-19',
       name: 'Aenean Ru Bristique 19',
+      image:
+        'https://images.pexels.com/photos/827518/pexels-photo-827518.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -181,6 +219,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-20',
       name: 'Aenean Ru Bristique 20',
+      image:
+        'https://images.pexels.com/photos/279618/pexels-photo-279618.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -190,6 +230,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-21',
       name: 'Aenean Ru Bristique 21',
+      image:
+        'https://images.pexels.com/photos/945688/pexels-photo-945688.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -199,6 +241,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-22',
       name: 'Aenean Ru Bristique 22',
+      image:
+        'https://images.pexels.com/photos/462235/pexels-photo-462235.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -208,6 +252,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-23',
       name: 'Aenean Ru Bristique 23',
+      image:
+        'https://images.pexels.com/photos/276583/pexels-photo-276583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       category: 'bed',
       price: 30,
       stars: 2,
@@ -217,6 +263,8 @@ const initialState = {
     {
       id: 'aenean-ru-bristique-24',
       name: 'Aenean Ru Bristique 24',
+      image:
+        'https://images.pexels.com/photos/1648768/pexels-photo-1648768.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       category: 'bed',
       price: 30,
       stars: 2,
