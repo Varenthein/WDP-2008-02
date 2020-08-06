1fa7b68a111ac1a020ea79172ba24a059e1f8f13
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-08-06 09:52:23 +0200

    Changed FeatureBoxes.js (divs to links for features) and FeatureBox.module.scss (hover effect for features).

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -11,6 +11,7 @@ const initialState = {
       id: 'aenean-ru-bristique-1',
       name: 'Aenean Ru Bristique 1',
       category: 'bed',
+      //image: 'https://i.postimg.cc/HL4mFXMs/pexels-photo-775219.jpg',
       price: 30,
       stars: 2,
       promo: 'sale',
