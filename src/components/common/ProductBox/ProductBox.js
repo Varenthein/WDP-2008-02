caac95b480539f75f2ac652844e13f808e4d0c3b
Kacper Wójtowicz <kacper.m.wojtowicz@gmial.com>
2020-08-12 13:53:29 +0200

    add StarRating component

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -3,15 +3,21 @@ import PropTypes from 'prop-types';
 
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faStar,
-  faExchangeAlt,
-  faShoppingBasket,
-} from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faExchangeAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+import StarRating from '../ProductBox/StarRating';
 
-const ProductBox = ({ name, price, promo, stars, image, oldPrice, favorite, compare }) => (
+const ProductBox = ({
+  name,
+  price,
+  promo,
+  stars,
+  image,
+  oldPrice,
+  favorite,
+  compare,
+}) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       <img src={image} alt={'coming soon'} className={styles.image} />
@@ -25,17 +31,7 @@ const ProductBox = ({ name, price, promo, stars, image, oldPrice, favorite, comp
     </div>
     <div className={styles.content}>
       <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
+      <StarRating />
     </div>
     <div className={styles.line}></div>
     <div className={styles.actions}>
