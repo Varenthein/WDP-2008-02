73608d2a76aea3c2ef6df04a4bc64d0f726a9001
Marcelina <m.oscislowska@gmail.com>
2020-08-17 21:37:10 +0200

    Add a sorting selectors

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -1,9 +1,18 @@
 /* selectors */
 export const getAll = ({ products }) => products;
 export const getCount = ({ products }) => products.length;
+export const price = ({ products }) => products.price;
 
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
+
+export const getLimitedProducts = ({ products }, limit) => products.slice(0, limit);
+
+export const sortAscByParam = ({ products }, price) =>
+  products.sort((a, b) => a.price - b.price);
+
+export const sortDescByParam = ({ products }, price) =>
+  products.sort((a, b) => b.price - a.price);
 
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
